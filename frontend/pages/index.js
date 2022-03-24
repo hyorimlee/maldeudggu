9c05dd757cf97951020ca713d4ebb38c635e5ee9
yoon <jiyoonhan12@gmail.com>
2022-03-24 15:53:23 +0900

    [S06P22A203-110] feat : initial setting

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+      </main>
+
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  )
+}
