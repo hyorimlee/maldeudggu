f977c11ff24012f342aa4ac0039e3dcbadaffcf2
yoon <jiyoonhan12@gmail.com>
2022-03-24 17:25:47 +0900

    [S06P22A203-111] feat : CustomInput initialize

---
+++ frontend/components/input/CustomInput.js
@@ -0,0 +1,13 @@
+import { Input } from '@nextui-org/react'
+
+export default function CustomInput() {
+  return (
+    <>
+      <Input
+        clearable
+        underlined
+        labelPlaceholder="닉네임"
+      />
+    </>
+  )
+}
