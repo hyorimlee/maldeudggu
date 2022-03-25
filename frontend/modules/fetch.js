37cb9f5041c43bbda741274d5762029a59ae5e47
unho-00 <djsgh94@naver.com>
2022-03-25 16:12:55 +0900

    [S06P22A203-113] feat : fetch module implement

---
+++ frontend/modules/fetch.js
@@ -0,0 +1,97 @@
+const SERVER_BASE = process.env.NEXT_PUBLIC_SERVER_BASE
+
+/*
+- Description
+  각종 GET 요청
+
+- input
+  url : 요청 주소
+  params : 요청 파라미터 (타입: 객체) (ex. { name='Alex', age=14 })
+
+- output
+  요청 결과 반환 json 파일
+*/
+async function getRequest(url, params={}) {
+  try {
+    const paramsKeys = Object.keys(params)
+    let query
+    
+    if (paramsKeys.length) {
+      query = paramsKeys.map(k => `${k}=${params[k]}`).join('&')
+    }
+
+    const response = await fetch(`${SERVER_BASE}${url}${paramsKeys.length ? '?' + query : ''}`)
+    
+    if (response.ok) {
+      const data = await response.json()
+      return data
+    }
+    console.log('fetch 모듈 응답 Not Ok')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+/*
+- Description
+  각종 POST 요청
+
+- input
+  url : 요청 주소
+  datas : post와 함께 보낼 데이터
+
+- output
+  요청 결과 json 파일
+*/
+async function postRequest(url, datas=[]) {
+  try {
+    let formData = new FormData()
+    datas.forEach(data => formData.append(data))
+
+    const response = await fetch(`${SERVER_BASE}${url}`, {
+      method: 'POST',
+      body: formData
+    })
+    
+    if (response.ok) {
+      const data = await response.json()
+      return data
+    }
+    console.log('fetch 모듈 응답 Not Ok')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+/*
+- Description
+  공유 이미지 저장 요청 함수
+
+- input
+  url : 요청 주소
+  image : 저장할 이미지 파일 (타입: File 객체)
+
+- output
+  저장 성공 여부 json 파일
+*/
+async function imageSave(url, image) {
+  try {
+    let formData = new FormData()
+    formData.append(image)
+
+    const response = await fetch(`${SERVER_BASE}${url}`, {
+      method: 'PATCH',
+      body: formData
+    })
+    
+    if (response.ok) {
+      const data = await response.json()
+      return data
+    }
+    console.log('fetch 모듈 응답 Not Ok')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export { getRequest, postRequest, imageSave }
