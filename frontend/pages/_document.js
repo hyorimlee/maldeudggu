34ddfc2e38f39b63f350aa612e5542457703a26f
unho-00 <djsgh94@naver.com>
2022-03-25 12:22:20 +0900

    [S06P22A203-113] style : code style change 1. 코드 스타일링 통일

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
+function Document() {
   return (
     <Html>
       <Head>
@@ -15,3 +15,5 @@ export default function Document() {
     </Html>
   )
 }
+
+export default Document
--- frontend/pages/customize.js
+++ frontend/pages/customize.js
@@ -1,6 +1,8 @@
-export default function Customize() {
+function Customize() {
   return (
     <>
     </>
   )
 }
+
+export default Customize
