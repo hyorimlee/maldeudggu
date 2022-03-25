8d2f9466f140a1d34285eb4e621f11612695eb63
hyorimlee <hl1046@pusan.ac.kr>
2022-03-25 15:23:35 +0900

    [S06P22A203-94] feat:add decorator for query params

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,9 +19,6 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-schema_url_patterns=[
-    path('api/v1/dialects/', include('dialects.urls')),
-]
 schema_view = get_schema_view(
     openapi.Info(
         title="말듣쓰",
@@ -31,8 +28,6 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
-    patterns=schema_url_patterns,
-
 )
 
 urlpatterns = [
