95012e1dc033061b80ea9caa7cedd3c37826a9e2
hyorimlee <hl1046@pusan.ac.kr>
2022-03-25 10:16:09 +0900

    [S06P22A203] feat : add return value for test

--- backend/dialects/views.py
+++ backend/dialects/views.py
@@ -5,29 +5,31 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import CaseSerializer, SentenceSerializer
 from .models import *
-
+from django.http import Http404
 # Create your views here.
 
 @api_view()
 def image_list(request):
-    """
-    image_list = Case.objects.filter(image_url__isnull=False)
-    serializer = CaseSerializer(image_list, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-    """
-
+    
+    # image_list = Case.objects.filter(image_url__isnull=False)
+    # serializer = CaseSerializer(image_list, many=True)
+    # if not serializer:
+    #     raise Http404("[image_list]No Case matches the given query")
+    # return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    return Response("hello image_list",status=status.HTTP_200_OK)
 def count_participant(request):
     """
     tb_case에서 result != Null 인 데이터 개수
     """
-    return
+    return Response("hello count_participant",status=status.HTTP_200_OK)
 
 def start_test(request):
     """
     - tb_case에 튜플 생성
     - 케이스 pk와 랜덤 문장 리스트 반환
     """
-    return
+    return Response("hello start_test",status=status.HTTP_200_OK)
 
 def save_audio(request):
     """
@@ -36,7 +38,7 @@ def save_audio(request):
     case pk 이름의 폴더 아래에, 날짜시각, sentence pk, 확장자로 이루어진 파일
     저장된 audio pk 리턴
     """
-    return
+    return Response("hello save_audio",status=status.HTTP_200_OK)
 
 def get_result(request):
     """
@@ -44,11 +46,11 @@ def get_result(request):
     case pk에 해당하는 오디오파일 뽑아서 모델에 돌리고 / 로컬 스토리지에서 한번에 받고
     case pk 에 해당하는 결과 반환하며 tb_case에 결과값 저장 (리스트 형태. 저장시 string)
     """
-    return
+    return Response("hello get_result",status=status.HTTP_200_OK)
 
 def save_image(request):
     """
     클라이언트에서 firebase 이미지 url과 case pk 주면, 저장(patch)
     ok message (+ 결과값) 반환
     """
-    return
+    return Response("hello save_image",status=status.HTTP_200_OK)
