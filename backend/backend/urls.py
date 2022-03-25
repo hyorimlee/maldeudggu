3c61722107147fdc0283a2e722828ba00e082c00
hyorimlee <hl1046@pusan.ac.kr>
2022-03-25 11:32:35 +0900

    [S06P22A203-94] feat:add setting for auto API-Docs generator

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# for api docs
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+schema_url_patterns=[
+    path('api/v1/dialects/', include('dialects.urls')),
+]
+schema_view = get_schema_view(
+    openapi.Info(
+        title="말듣쓰",
+        default_version='v0',
+        description="API DOCS",
+        terms_of_service="https://www.google.com/policies/terms/",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    patterns=schema_url_patterns,
+
+)
 
 urlpatterns = [
+    path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
     path('admin/', admin.site.urls),
     path('api/v1/dialects/', include('dialects.urls')),
 ]
