14bea3c0d6cc858fb805be982f7346e9d9a24fa3
unho-00 <djsgh94@naver.com>
2022-03-28 14:47:35 +0900

    [S06P22A203-113] fix : bug fix

--- frontend/containers/sharedImages/sharedImages.js
+++ frontend/containers/sharedImages/sharedImages.js
@@ -4,8 +4,9 @@ import styles from './sharedImages.module.css'
 
 export async function getServerSideProps() {
   // 백엔드 서버에서 저장된 이미지를 로드해와야 함
-  const data = await getRequest('/api/v1/dialects/shared')
-  console.log(data)
+  const response = await getRequest('/shared')
+  const data = await response.json()
+  
   
   return {
     props : {
