a19ebdb21b1d92a86863f2b412f8d8e48250c41f
Choux <choifrance@gmail.com>
2022-03-31 23:34:47 +0900

    [S06P22A203-189] update : add loading to record page

--- frontend/pages/[recordId]/index.js
+++ frontend/pages/[recordId]/index.js
@@ -3,50 +3,73 @@ import Button from "../../components/button/button"
 import styles from "../../styles/record.module.css"
 import RecordButton from "../../components/button/recordButton";
 import AudioProgressBar from "../../components/progressBar/audioProgressBar";
-import { postRequest } from "../../modules/fetch";
+import { getRequest, postRequest } from "../../modules/fetch";
 import { useState } from "react";
+import { useRouter } from "next/router";
+import LoadingSlide from "../../components/loading/loadingslide";
 
 function Record( {staticState, changeStaticState} ) {
   const [curSentence, setCursentence] = useState(0)
+  const [isRecordEnd, setIsRecordEnd] = useState(false)
+  const router = useRouter()
   const sentences = staticState.sampleSentence.sentences
   const caseId = staticState.sampleSentence.case_id
   let soundfiles = staticState.recordAudioFile
   let soundfile = soundfiles[soundfiles.length - 1]
 
   async function sendSoundFile() {
-    if (curSentence === 4) {
-      console.log('로딩페이지로 넘어가기')
-    } else {
+    if (curSentence < 4) {
       await postRequest(`/${caseId}/?sentence=${sentences[curSentence].id}`, [["audio", soundfile]])
       setCursentence(curSentence + 1)
+    } else {
+      await postRequest(`/${caseId}/?sentence=${sentences[curSentence].id}`, [["audio", soundfile]])
+      setIsRecordEnd(true)
+      const testResult = await getRequest(`/${caseId}/result/?reuse=true`)
+      setTimeout(() => {
+        router.push('/result')
+      }, 5000)
     }
   }
 
   return (
     <>
-      <Text
-        size={16}
-        contents={`${curSentence + 1}/5`}
-      ></Text>
-      <Text
-        size={16}
-        contents={'아래의 문장을 평소 말투로 녹음해주세요'}
-      ></Text>
-      <Text
-        color={'white'}
-        size={18}
-        contents={`${sentences[curSentence].sentence}`}
-      ></Text>
-      <RecordButton sentenceId={`${sentences[curSentence].id}`} staticState={staticState} changeStaticState={(type, data) => {
-        console.log(changeStaticState)
-        changeStaticState(type, data)
-      }}/>
-      <AudioProgressBar staticState={staticState} />
-      <Button
-        content={'다음으로 넘어가기'}
-        color={'grey'}
-        handler={sendSoundFile}
-      />
+      <div>
+        { isRecordEnd ?
+          <div>
+            <LoadingSlide />
+            <Text
+              size={16}
+              contents={'결과를 측정하고 있습니다'}
+            ></Text>
+          </div>
+          :
+          <div>
+            <Text
+              size={16}
+              contents={`${curSentence + 1}/5`}
+            ></Text>
+            <Text
+              size={16}
+              contents={'아래의 문장을 평소 말투로 녹음해주세요'}
+            ></Text>
+            <Text
+              color={'white'}
+              size={18}
+              contents={`${sentences[curSentence].sentence}`}
+            ></Text>
+            <RecordButton sentenceId={`${sentences[curSentence].id}`} staticState={staticState} changeStaticState={(type, data) => {
+              console.log(changeStaticState)
+              changeStaticState(type, data)
+            }}/>
+            <AudioProgressBar staticState={staticState} />
+            <Button
+              content={'다음으로 넘어가기'}
+              color={'grey'}
+              handler={sendSoundFile}
+            />
+          </div>
+        }
+      </div>
     </>
   )
 }
