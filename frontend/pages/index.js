a4d813646a6da0b781f10d72cf7008a350f6cacc
yoon <jiyoonhan12@gmail.com>
2022-04-01 15:02:09 +0900

    [S06P22A203-170] feat : add participant

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -15,20 +15,20 @@ import { faAnglesDown } from "@fortawesome/free-solid-svg-icons"
 import { getRequest } from '../modules/fetch'
 
 import { postRequest } from "../modules/fetch"
-/*
-사람들 공유 이미지 가져오기
-*/
+
+// 공유 이미지 & 참여자 수 가져오기
 export async function getServerSideProps() {
   const data = await getRequest('/shared')
-
+  const participant = await getRequest('/participant')
   return {
     props: {
-      data
+      data,
+      participant
     }
   }
 }
 
-function Home({ staticState, changeStaticState, data }) {
+function Home({ staticState, changeStaticState, data, participant }) {
   const [agree, setAgree] = useState(false)
   const router = useRouter()
 
@@ -51,7 +51,7 @@ function Home({ staticState, changeStaticState, data }) {
       ></Text>
       <div className={styles.temp}>로고 자리</div>
       <Text
-        contents={'지금까지 nn명이 참여했어요!'}
+        contents={`지금까지 ${participant.count}명이 참여했어요!`}
       ></Text>
       <Text
         contents={'말듣꾸는 나의 평소 말투를 인공지능이 분석하여 어느 지방의 억양을 사용하고 있는지 알려주는 서비스입니다.'}
@@ -63,7 +63,7 @@ function Home({ staticState, changeStaticState, data }) {
       <Checkbox
         checked={agree}
         onChange={checkHandler}
-        contents={'음성 데이터 수집에 동의합니다.'}
+        contents={'(필수) 음성 데이터 활용에 동의합니다.'}
       ></Checkbox>
       <Button
         content={'테스트 시작하기'}
