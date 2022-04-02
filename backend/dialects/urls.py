241bd064ccd52382651377ed6079a0cfcb84d7ed
root <root@ip-172-26-13-216.ap-northeast-2.compute.internal>
2022-04-02 06:54:09 +0000

    feat: download image

--- backend/dialects/urls.py
+++ backend/dialects/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('<int:case_pk>/', views.save_audio),
     path('<int:case_pk>/result/', views.get_result),
     path('<int:case_pk>/image/', views.save_image),
+    path('<int:case_pk>/download/', views.download_image)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
