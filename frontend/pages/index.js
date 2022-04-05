07c70227731882198a32d3bda3565eb637cb6bd3
Choux <choifrance@gmail.com>
2022-04-05 15:15:39 +0900

    [S06P22A203-237] feat : add send survey request & next head

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef, lazy, Suspense } from "react"
 import { useRouter } from "next/router"
+import Head from "next/head"
 
 // components & containers
 import Text from "../components/text/text"
@@ -80,9 +81,22 @@ function Home({ staticState, changeStaticState }) {
     const response = await postRequest('/start/', [['nickname', nickname.trim()]])      // 닉네임 양끝 공백 제거
     changeStaticState('sentences', response.sentences, 'caseId', response.case_id)
 
-    setTimeout(() => {
-      setDelay(true)
-    }, 1000 + Math.random() * 1000)
+    if (staticState.reuse) {
+      let metaData = Object.entries(staticState.metaData).map(data => [data[0], parseInt(data[1])])
+      postRequest(`/${response.case_id}/survey/`, metaData).then(
+        setTimeout(() => {
+          setDelay(true)
+        }, 1000 + Math.random() * 1000)
+      ).catch(
+        setTimeout(() => {
+          setDelay(true)
+        }, 1000 + Math.random() * 1000)
+      )
+    } else {
+      setTimeout(() => {
+        setDelay(true)
+      }, 1000 + Math.random() * 1000)
+    }
   }
 
   useEffect(() => {
@@ -93,6 +107,16 @@ function Home({ staticState, changeStaticState }) {
 
   return (
     <>
+      <Head>
+        <title>말듣꾸 - AI방언분석</title>
+        <meta
+          name="description"
+          content="말듣꾸는 한국어 사용자의 음성을 인공지능을 통해 분석하여 어느 지방의 사투리를 사용하는지 알려주는 서비스입니다. 내가 평소에 쓰는 억양은 어느 지방의 억양에 가까운지 한 번 알아보세요!"
+        />
+        <meta name="keywords" content="AI, 사투리, 방언, 음성분석, 캐릭터"/>
+        <meta property="og:title" content="말듣꾸 - AI방언분석" />
+        <meta property="og:description" content="AI 방언분석 서비스 말듣꾸 입니다. 내가 평소에 쓰는 억양은 어느 지방의 억양에 가까운지 한 번 알아보세요!" />
+      </Head>
       {delay ? (
         <ThreeDotsWave
           contents={'테스트를 준비중이에요.'}
